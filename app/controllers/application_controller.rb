9d1b7033589961ee88212dc9e51b31e22b7b1140
Max <maxmwangikenya@gmail.com>
2023-06-20 15:49:09 +0300

    completed assignment

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,7 @@
-class ApplicationController < ActionController::API
-end
+class StudentsController < ApplicationController
+    def index
+      students = Student.all
+      render json: students
+    end
+  end
+  
